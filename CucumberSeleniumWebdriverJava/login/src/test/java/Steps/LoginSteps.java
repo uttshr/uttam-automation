be6659d4fd2c4ee6610c0c7d9b8c5665a2fc560f
uttshr <uttshr@gmail.com>
2022-09-12 21:15:36 +0545

    commit on 12 Sep 2022

---
+++ CucumberSeleniumWebdriverJava/login/src/test/java/Steps/LoginSteps.java
@@ -0,0 +1,19 @@
+package Steps;
+
+public class LoginSteps {
+    @io.cucumber.java.en.Given("^I navigate to website$")
+    public void iNavigateToWebsite() {
+    }
+
+    @io.cucumber.java.en.And("^I enter <username> and <password>$")
+    public void iEnterUsernameAndPassword() {
+    }
+
+    @io.cucumber.java.en.And("^I click login button$")
+    public void iClickLoginButton() {
+    }
+
+    @io.cucumber.java.en.Then("^I should see dashboard$")
+    public void iShouldSeeDashboard() {
+    }
+}
