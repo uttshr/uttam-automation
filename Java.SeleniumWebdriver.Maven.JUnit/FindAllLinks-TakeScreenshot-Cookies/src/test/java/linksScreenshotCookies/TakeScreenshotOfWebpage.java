fd8a649b138bcc85b0bc802014bc5b231dfb126c
uttshr <uttshr@gmail.com>
2024-10-07 13:36:03 +0545

    new code push on 07 Oct 2024

---
+++ Java.SeleniumWebdriver.Maven.JUnit/FindAllLinks-TakeScreenshot-Cookies/src/test/java/linksScreenshotCookies/TakeScreenshotOfWebpage.java
@@ -0,0 +1,46 @@
+package linksScreenshotCookies;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
+public class TakeScreenshotOfWebpage {
+	
+	WebDriver driver;
+	
+	@Before
+	public void Initialize() {
+        driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/watch?v=KloahKhSWTk");
+	}
+	
+	@Test
+	public void TestRun() {
+		    // Capture screenshot
+		 File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+	        try {
+	            // Save the screenshot to a specific location
+	            FileHandler.copy(screenshot, new File("C:/Users/Acer/Downloads/screenshot.png"));
+	            System.out.println("Screenshot saved successfully.");
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	}
+	
+	@After
+	public void TearDown() {
+	        // Close the browser
+	        driver.quit();
+	    }
+
+}
